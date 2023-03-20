f0d1e93d366c2fbaa92f08cfc0cd26358ea78db7
Ferencz Kincső <ferenczkincso@gmail.com>
2023-03-20 18:45:49 +0100

    Fix 7.5

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -1,5 +1,7 @@
 package hu.bme.mit.train.system;
 
+import javax.swing.plaf.basic.BasicTabbedPaneUI.TabSelectionHandler;
+import hu.bme.mit.train.controller.Tachograph;
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
@@ -7,11 +9,15 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+import java.util.Date;
+
+
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+	private Tachograph tach = new Tachograph ();
 
 	public TrainController getController() {
 		return controller;
@@ -25,4 +31,12 @@ public class TrainSystem {
 		return user;
 	}
 
+	public Tachograph getTachograph(){
+		tach.addData(new Date(), 1,1);
+		tach.addData(new Date(),0,0);
+		return tach;
+	}
+
+	
 }
+
